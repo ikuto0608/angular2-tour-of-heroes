0082d431c9944574bcf3171f6061803ab255ed5a
ikuto <ikuto0608@me.com>
2016-08-10 16:01:36 -0700

    first commit

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'RubberMan' },
+  { id: 16, name: 'Dynama' },
+  { id: 17, name: 'Dr IQ' },
+  { id: 18, name: 'Magma' },
+  { id: 19, name: 'Tornade' },
+  { id: 20, name: 'Thander' },
+];
